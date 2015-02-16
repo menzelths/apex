d8892dff6bd6562a63b7918549b5b2d16c110b9f
Thomas Menzel <menzelths@yahoo.de>
2015-02-16 15:45:22 +0100

    test basic functionality

---
+++ apex-test/src/main/java/de/qreator/apex/test/HelloWorld.java
@@ -0,0 +1,21 @@
+package de.qreator.apex.test;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.apex.Router;
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class HelloWorld {
+
+  public static void main(String[] args) {
+
+    Vertx vertx = Vertx.vertx();
+
+    Router router = Router.router(vertx);
+    router.route().handler(rc -> rc.response().putHeader("content-type", "text/plain").end("Hallo Leute!"));
+
+    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+    
+  }
+}
